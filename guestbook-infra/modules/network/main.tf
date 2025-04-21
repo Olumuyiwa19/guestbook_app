a1ccf14d111df1d38664f7386ef772014c42dffe
Olumuyiwa19 <kolayemi.olumuyiwa@gmail.com>
2025-04-21 12:12:55 -0400

    provision network foundation infra - VPC, Subnets, IGW, EGW,Route-Tables

--- guestbook-infra/modules/network/main.tf
+++ guestbook-infra/modules/network/main.tf
@@ -21,7 +21,7 @@ resource "aws_subnet" "public" {
   count                   = length(var.public_subnet_cidrs)
   vpc_id                  = aws_vpc.main.id
   cidr_block              = var.public_subnet_cidrs[count.index]
-  availability_zone       = data.aws_availability_zones.available[count.index]
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
   map_public_ip_on_launch = true
   tags = {
     Name = "${var.app_name}-public-${count.index}"
@@ -32,7 +32,7 @@ resource "aws_subnet" "private" {
   count             = length(var.private_subnet_cidrs)
   vpc_id            = aws_vpc.main.id
   cidr_block        = var.private_subnet_cidrs[count.index]
-  availability_zone = data.aws_availability_zones.available[count.index]
+  availability_zone = data.aws_availability_zones.available.names[count.index]
   tags = {
     Name = "${var.app_name}-private-${count.index}"
   Environment = var.environment }
